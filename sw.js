fd29fa8307226b66a4620504c5cd8efa6ebbdb6d
Bobbi Trehan-Young <bobbi_trehan@hotmail.com>
2026-05-14 20:33:03 +0100

    Fixed the two bugs
    NFC tag not writing
    Native Mobile OS share feature

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "bobbi-card-v2";
+const CACHE = "bobbi-card-v4";
 const FILES = ["./", "./index.html", "./manifest.json", "./profile.jpg", "./icon-192.png", "./icon-512.png"];
 
 self.addEventListener("install", (e) => {
